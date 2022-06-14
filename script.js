140f47c310a0c379f9322baffdd728fa9ae4d86e
SleepingWarrior <fpetersen2@gmail.com>
2022-06-14 20:27:17 +0200

    navbar js added

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
-$(document).ready(() => {});
+$(document).ready(() => {
+  $("#menu").click(function () {
+    $(this).toggleClass("fa-times");
+    $("header").toggleClass("toggle");
+  });
 
-const animation = () => {
-  $("#about-btn, #email-btn").fadeTo(1000, 0.2).css("background", "#333").fadeTo(1000, 1.0, animation);
-};
+  $(window).on("scroll load", function () {
+    $("#menu").removeClass("fa-times");
+    $("header").removeClass("toggle");
 
-$("#about-btn, #email-btn").on("mouseover", () => {
-  $("#about-btn, #email-btn").stop(true).fadeTo(1.0, 1.0).css("background", "var(--yellow)");
+    if ($(window).scrollTop() > 0) {
+      $(".top").show();
+    } else {
+      $(".top").hide();
+    }
+  });
+
+  // smooth scrolling
+
+  $('a[href*="#"]').on("click", function (e) {
+    e.preventDefault();
+
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top,
+      },
+      500,
+      "linear"
+    );
+  });
+
+  const animation = () => {
+    $("#about-btn, #email-btn").fadeTo(1000, 0.2).css("background", "#333").fadeTo(1000, 1.0, animation);
+  };
+
+  $("#about-btn, #email-btn").on("mouseover", () => {
+    $("#about-btn, #email-btn").stop(true).fadeTo(1.0, 1.0).css("background", "var(--yellow)");
+  });
+
+  $("#about-btn, #email-btn").on("mouseout", animation);
+
+  animation();
 });
-
-$("#about-btn, #email-btn").on("mouseout", animation);
-
-animation();
